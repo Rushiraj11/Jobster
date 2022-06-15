764f316fb7636feb47d8739dc84ff3f350ac300a
Rushiraj Ghatge <rushiraj.ghatge@novigosolutions.com>
2022-06-15 15:42:00 +0530

    Added: Landing page to the application

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Landing from "./pages/Landing";
+import styled from 'styled-components';
+
+
+// const Button = styled.button`
+// background:red;
+// color:white;`
+
+// const Secondbutton = styled.button`
+//   background: green;
+//   color:white;
+// `;
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+    {/* <Button>click</Button>
+    <Secondbutton>Press me</Secondbutton> */}
+     <Landing />
+   </div>
   );
 }
 
